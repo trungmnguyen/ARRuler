39de5165b9f948f8c06ad3f049113c6e0fa6d7f6
trungnguyen <trungnguyen2171992@gmail.com>
2019-01-14 22:00:07 -0800

    Adding a start and end measuring points in a 3D environment

---
+++ ARRuler/Controllers/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  ARRuler
+//
+//  Created by TRUNG NGUYEN on 1/14/19.
+//  Copyright © 2019 TRUNG NGUYEN. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        //Set Debug Option
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //        print("Touch Detected")
+        
+        if let touchLocation = touches.first?.location(in: sceneView){
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    func addDot(at hitResult: ARHitTestResult){
+        let dotGeometry = SCNSphere(radius: 0.005)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(
+            x: hitResult.worldTransform.columns.3.x,
+            y: hitResult.worldTransform.columns.3.y,
+            z: hitResult.worldTransform.columns.3.z)
+        
+       sceneView.scene.rootNode.addChildNode(dotNode)
+        
+    }
+    
+    
+}
